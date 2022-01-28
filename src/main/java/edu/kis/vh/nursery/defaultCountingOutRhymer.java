006d1e474aeeb3de3ac8e58a8cb2d8858a9590c2
Kamil <kamildymecki2000@interia.pl>
2022-01-28 11:17:41 +0100

    1.5 8 Zgłoszenie rozwiązania (review)

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,34 +1,64 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+/**
+ * Implementacja klasy
+ *
+ */
+public class DefaultCountingOutRhymer {
 
-	private int[] NUMBERS = new int[12];
+    private static final int NUMBERS_SIZE = 12;
+    private static final int TOTAL_VAL = -1;
+    private static final int CALL_CHECK_RETURN_VAL = -1;
 
-	public int total = -1;
-
-	public void countIn(int in) {
-		if (!isFull())
-			NUMBERS[++total] = in;
-	}
-
-		public boolean callCheck() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return NUMBERS[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return NUMBERS[total--];
-					}
+    private int total = TOTAL_VAL;
+    private final int[] numbers = new int[NUMBERS_SIZE];
+    //tablica numbers została zmieniona na final podczas poprzednich laboratoriów
+    /**
+     *
+     * @return stack total value
+     */
+    public int getTotal() {
+        return total;
+    }
+    /**
+     * adding element to stack
+     * @param in value to put on the stack
+     */
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+    /**
+     * check if stack is empty
+     * @return true is empty, false not empty
+     */
+    public boolean callCheck() {
+        return total == TOTAL_VAL;
+    }
+    /**
+     * check if stack is full
+     * @return true is full, false not full
+     */
+    public boolean isFull() {
+        return total == 11;
+    }
+    /**
+     *getting stack top
+     * @return last element of stack
+     */
+    protected int peekaboo() {
+        if (callCheck())
+            return CALL_CHECK_RETURN_VAL;
+        return numbers[total];
+    }
+    /**
+     * deleting stack top
+     * @return last element of stack
+     */
+    public int countOut() {
+        if (callCheck())
+            return CALL_CHECK_RETURN_VAL;
+        return numbers[total--];
+    }
 
 }
